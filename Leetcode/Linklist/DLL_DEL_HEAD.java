8f1c8fc8cac239e06d1a103621e9b4dc0ce9be9a
Shadia Naaz <shadianaaz17@gmail.com>
2025-07-27 23:31:55 +0530

    dll head del

---
+++ Leetcode/Linklist/DLL_DEL_HEAD.java
@@ -0,0 +1,39 @@
+/*Definition of doubly linked list:
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode prev;
+
+    ListNode() {
+        val = 0;
+        next = null;
+        prev = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+        prev = null;
+    }
+
+    ListNode(int data1, ListNode next1, ListNode prev1) {
+        val = data1;
+        next = next1;
+        prev = prev1;
+    }
+}
+ */
+
+class Solution {
+    public ListNode deleteHead(ListNode head) {
+      ListNode temp=head;
+      head=head.next;
+      head.prev=null;
+      temp.next=null;
+      return head;
+
+
+
+
+    }
+}
